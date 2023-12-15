042b8ae49d2a4f1dd764d99bb3aba18dacb7082a
GabrielSZordan <115323492+GabrielSZordan@users.noreply.github.com>
2023-12-15 19:27:07 -0300

    Add files via upload

---
+++ aula-08-tdd/src/main/java/br/inatel/cdg/aula_08/achievementex4/RecebedorMSG.java
@@ -0,0 +1,7 @@
+package br.inatel.cdg.aula_08.achievementex4;
+
+public interface RecebedorMSG {
+
+	void recebedorMensagem(String msg);
+
+}
---
+++ aula-08-tdd/src/main/java/br/inatel/cdg/aula_08/achievementex4/SemRecebedoresException.java
@@ -0,0 +1,9 @@
+package br.inatel.cdg.aula_08.achievementex4;
+
+public class SemRecebedoresException extends RuntimeException {
+
+	public SemRecebedoresException(String message) {
+		super(message);
+	}
+	
+}
